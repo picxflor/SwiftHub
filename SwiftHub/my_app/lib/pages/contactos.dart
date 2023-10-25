4c25a8c1494fcfc51a8d8bbd4d8a791cdc5e0782
franktc135 <frankduxtc135@gmail.com>
2023-10-25 12:20:33 -0500

    contactos
    contactos

---
+++ SwiftHub/my_app/lib/pages/contactos.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: ContactPage(),
+    );
+  }
+}
+
+
+class ContactPage extends StatefulWidget {
+  const ContactPage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _ContactPageState createState() => _ContactPageState();
+}
+
+class _ContactPageState extends State<ContactPage> {
+  List<String> contacts = [
+    'Juan Perez',
+    'Maria Garcia',
+    'Luis Hernandez',
+    'Ana Lopez',
+    'Carlos Martinez',
+    'Sofia Rodriguez',
+    'Pedro Chavez',
+    'Laura Sanchez',
+    'Oscar Ramirez',
+    'Elena Flores',
+  ];
+
+  List<String> filteredContacts = [];
+
+  @override
+  void initState() {
+    filteredContacts = contacts;
+    super.initState();
+  }
+
+  void filterContacts(String query) {
+    setState(() {
+      filteredContacts = contacts
+          .where((contact) =>
+              contact.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            // Este es el boton de regresar
+          },
+        ),
+        title: const Text('Contactos'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+            keyboardType: TextInputType.number, // Configura el teclado para que 
+              onChanged: (value) => filterContacts(value),
+              decoration: const InputDecoration(
+                labelText: 'Buscar contacto',
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: filteredContacts.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  title: Text(filteredContacts[index]),
+                  onTap: () {
+                  },
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+        },
+        label: const Text('Llamar'),
+        icon: const Icon(Icons.phone),
+        backgroundColor: Colors.green,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,//posision del boton
+    );
+  }
+}
