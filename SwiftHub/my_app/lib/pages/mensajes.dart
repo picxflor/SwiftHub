744e34e60ee10f86a5a736b779f6682b2e07b32a
franktc135 <frankduxtc135@gmail.com>
2023-10-25 12:20:50 -0500

    mensajes
    mensajes

---
+++ SwiftHub/my_app/lib/pages/mensajes.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MySms());
+
+class MySms extends StatelessWidget {
+  const MySms({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: SmsApp (),
+    );
+  }
+}
+
+
+class SmsApp extends StatelessWidget {
+  const SmsApp ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+  
+      appBar: AppBar(//barra superior
+        
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            // Este es el boton de regresar
+          },
+        ),
+        title: const Text('Mensajes'),
+        //title: TextField(
+        //  decoration: InputDecoration(
+          //  hintText: 'Buscar...',
+          //border: InputBorder.none,
+        // ),
+        //  ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add_comment_outlined),
+            onPressed: () {
+              // Acciones para mensajes
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.mark_as_unread_rounded),
+            onPressed: () {
+              // Acciones para conversaciones
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.supervised_user_circle),
+            onPressed: () {
+              // Acciones para contactos
+            },
+          ),
+        ],
+      ),
+      //body:Column(
+        //children:[
+          //Padding(
+            //padding: const EdgeInsets.all(8.0),
+            //
+            //child: 
+              
+              //TextField( 
+              //decoration: const InputDecoration(
+               // hintText: 'Buscar...',
+                //),
+                //onChanged: (text) {
+                // Barra de busqueda limitada
+                //},
+              //),
+            //),
+          //],
+      //)
+      body: ListView(
+        children: List.generate(
+          10,
+          (index) => Container(
+            padding: EdgeInsets.all(8.0),
+            margin: EdgeInsets.all(8.0),
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.circular(12.0),
+            ),
+            child: ListTile(
+              title: Text('Mensaje $index'),
+              subtitle: Text('Contenido del mensaje $index'),
+            ),
+          ),
+        )
+      )
+    );
+  }
+}  
