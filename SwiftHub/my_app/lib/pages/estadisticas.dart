f0007a632c4a676d3ea8fd7e01d2e8538ce8bb8d
74768494 <112639656+74768494@users.noreply.github.com>
2023-11-04 18:29:43 -0500

    klk

--- SwiftHub/my_app/lib/pages/estadisticas.dart
+++ SwiftHub/my_app/lib/pages/estadisticas.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
 
 class EstadisPage extends StatelessWidget {
   const EstadisPage({super.key});
@@ -6,18 +7,61 @@ class EstadisPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 3, 106, 190),
       appBar: AppBar(
         title: Text('Estadísticas'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset('assets/images/grafico1.JPG'), 
-            Image.asset('assets/images/grafico2.JPG'), 
-            Image.asset('assets/images/grafico3.JPG'), 
-          ],
+      body: ListView(
+       children: [
+        Container(
+          margin: EdgeInsets.fromLTRB(0, 20, 0, 0),
+          child: Text('Recuento Mensual',style: TextStyle(
+          color: Colors.white,
+          fontSize: 30
         ),
+        textAlign:(TextAlign.center) ,
+        ),
+        ),
+        
+        Container(
+          margin: EdgeInsets.fromLTRB(0, 50, 0, 0),
+          child: CircularPercentIndicator(
+            radius: 200,
+            lineWidth: 20,
+            percent: 0.8,
+            progressColor: Colors.green,
+            backgroundColor: Colors.white,
+            circularStrokeCap: CircularStrokeCap.round,
+            center: Text('80%',style: TextStyle(fontSize: 50,color: Colors.white),),
+            ),
+        ),
+        Container(
+           margin: EdgeInsets.fromLTRB(0, 50, 0, 0),
+           
+          child: CircularPercentIndicator(
+            radius: 200,
+            lineWidth: 20,
+            percent: 0.3,
+            progressColor: Colors.red,
+            backgroundColor: Colors.white,
+            circularStrokeCap: CircularStrokeCap.round,
+            center: Text('30%',style: TextStyle(fontSize: 50,color: Colors.white),),
+            ),
+        ),
+         Container(
+           margin: EdgeInsets.fromLTRB(0, 50, 0, 50),
+           
+          child: CircularPercentIndicator(
+            radius: 200,
+            lineWidth: 20,
+            percent: 0.9,
+            progressColor: Colors.yellow,
+            backgroundColor: Colors.white,
+            circularStrokeCap: CircularStrokeCap.round,
+            center: Text('90%',style: TextStyle(fontSize: 50,color: Colors.white),),
+            ),
+        ),
+       ]
       ),
     );
   }
--- SwiftHub/my_app/lib/pages/profile_page.dart
+++ SwiftHub/my_app/lib/pages/profile_page.dart
@@ -22,197 +22,200 @@ class ProfilePage extends StatelessWidget {
         backgroundColor: Color.fromARGB(255, 31, 135, 219),
         title: Text('Mi Perfil'),
       ),
-       body: Center(
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                
-                children: [
-                  Container(
-                    child: Row(
-                       children:[ CircleAvatar(
-                    radius: 60,
-                    backgroundImage: AssetImage('assets/images/perfil.JPG'),
-                  ),
-                  Container(margin: EdgeInsets.fromLTRB(15, 0, 0, 0),
-                     child: Text(
-                    'Anderson Huaman',
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
+       body: ListView(
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  
+                  children: [
+                    Container(
+                      child: Row(
+                         children:[ CircleAvatar(
+                      radius: 60,
+                      backgroundImage: AssetImage('assets/images/perfil.JPG'),
                     ),
-                  ),
-                  )
-                 
-                       ],
+                    Container(margin: EdgeInsets.fromLTRB(15, 0, 0, 0),
+                       child: Text(
+                      'Anderson Huaman',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
+                    )
+                   
+                         ],
+                      ),
+                        
+                    ),
+                   
+                    Container(margin: EdgeInsets.all(15)),
+                  
+                    Container(
+                      margin: EdgeInsets.all(10),//Margen del primero boton
+                      child: ElevatedButton(onPressed: (){
+                         Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => LogradosPage()));
                       
-                  ),
-                 
-                  Container(margin: EdgeInsets.all(15)),
-                  Container(
-                    margin: EdgeInsets.all(10),//Margen del primero boton
-                    child: ElevatedButton(onPressed: (){
-                       Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => LogradosPage()));
-                    
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.green,
-                      fixedSize: Size(300, 75),
-                      shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20)
-          )
-                    ),
-                    child: Row(
-                      children: [
-                        Icon(Icons.check),
-                        Text('Logrados'),
-                         Container(
-              margin: EdgeInsets.fromLTRB(160, 0, 0, 0),
-              child: 
-                Icon(Icons.arrow_right),
-                
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.green,
+                        fixedSize: Size(300, 75),
+                        shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20)
             )
-                      ],
+                      ),
+                      child: Row(
+                        children: [
+                          Icon(Icons.check),
+                          Text('Logrados'),
+                           Container(
+                margin: EdgeInsets.fromLTRB(160, 0, 0, 0),
+                child: 
+                  Icon(Icons.arrow_right),
+                  
+              )
+                        ],
+                      ),
+                      ),
                     ),
-                    ),
-                  ),
-                   Container(
+                     Container(
       margin: EdgeInsets.all(10), // Margin of 20 for the second button
       child: ElevatedButton(
-        onPressed: () {
-            Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => PendientesPage()));
-        },
-        style: ElevatedButton.styleFrom(
-          fixedSize: Size(300, 75),
-          backgroundColor: Colors.red,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20)
-          )
-        ),
-        child: Row(
-          children: [
-            Icon(Icons.clear),
-            Text('Pendientes'),
-             Container(
-              margin: EdgeInsets.fromLTRB(145, 0, 0, 0),
-              child: 
-                Icon(Icons.arrow_right),
-                
+          onPressed: () {
+              Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => PendientesPage()));
+          },
+          style: ElevatedButton.styleFrom(
+            fixedSize: Size(300, 75),
+            backgroundColor: Colors.red,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20)
             )
-          ],
-        ),
+          ),
+          child: Row(
+            children: [
+              Icon(Icons.clear),
+              Text('Pendientes'),
+               Container(
+                margin: EdgeInsets.fromLTRB(145, 0, 0, 0),
+                child: 
+                  Icon(Icons.arrow_right),
+                  
+              )
+            ],
+          ),
       ),
     ),
     Container(
       margin: EdgeInsets.all(10), // Margin of 20 for the third button
       child: ElevatedButton(
-        onPressed: () {
-          Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => ImportantesPage()));
-        },
-        style: ElevatedButton.styleFrom(
-          fixedSize: Size(300, 75),
-          backgroundColor: Colors.yellow,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20)
-          )
-        ),
-        child: Row(
-          children: [
-            Icon(Icons.star),
-            Text('Importantes'),
-             Container(
-              margin: EdgeInsets.fromLTRB(140, 0, 0, 0),
-              child: 
-                Icon(Icons.arrow_right),
-                
+          onPressed: () {
+            Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ImportantesPage()));
+          },
+          style: ElevatedButton.styleFrom(
+            fixedSize: Size(300, 75),
+            backgroundColor: Colors.yellow,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20)
             )
-          ],
-        ),
+          ),
+          child: Row(
+            children: [
+              Icon(Icons.star),
+              Text('Importantes'),
+               Container(
+                margin: EdgeInsets.fromLTRB(140, 0, 0, 0),
+                child: 
+                  Icon(Icons.arrow_right),
+                  
+              )
+            ],
+          ),
       ),
     ),
     Container(
       margin: EdgeInsets.all(10), // Margin of 20 for the fourth button
       child: ElevatedButton(
-        onPressed: () {
-          Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => EstadisPage()));
-        },
-        style: ElevatedButton.styleFrom(
-          fixedSize: Size(300, 75),
-          backgroundColor: Colors.purple,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20)
-          )
-        ),
-        child: Row(
-          children: [
-            Icon(Icons.bar_chart),
-            Text('Estadisticas'),
-             Container(
-              margin: EdgeInsets.fromLTRB(140, 0, 0, 0),
-              child: 
-                Icon(Icons.arrow_right),
-                
+          onPressed: () {
+            Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => EstadisPage()));
+          },
+          style: ElevatedButton.styleFrom(
+            fixedSize: Size(300, 75),
+            backgroundColor: Colors.purple,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20)
             )
-          ],
-        ),
+          ),
+          child: Row(
+            children: [
+              Icon(Icons.bar_chart),
+              Text('Estadisticas'),
+               Container(
+                margin: EdgeInsets.fromLTRB(140, 0, 0, 0),
+                child: 
+                  Icon(Icons.arrow_right),
+                  
+              )
+            ],
+          ),
       ),
     ),
     Container(
       
       margin: EdgeInsets.all(10), // Margin of 20 for the fifth button
       child: ElevatedButton(
-        onPressed: () {
-         Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => ConfigPage()));
-        },
-        style: ElevatedButton.styleFrom(
-          fixedSize: Size(300, 75),
-          backgroundColor: Colors.black,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20)
-          )
-        ),
-        child: Row(
-          children: [
-            Icon(Icons.settings),
-            Text('Configuraciones'),
-            Container(
-              margin: EdgeInsets.fromLTRB(115, 0, 0, 0),
-              child: 
-                Icon(Icons.arrow_right),
-                
+          onPressed: () {
+           Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ConfigPage()));
+          },
+          style: ElevatedButton.styleFrom(
+            fixedSize: Size(300, 75),
+            backgroundColor: Colors.black,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20)
             )
-            
-          ],
-        ),
+          ),
+          child: Row(
+            children: [
+              Icon(Icons.settings),
+              Text('Configuraciones'),
+              Container(
+                margin: EdgeInsets.fromLTRB(115, 0, 0, 0),
+                child: 
+                  Icon(Icons.arrow_right),
+                  
+              )
+              
+            ],
+          ),
       ),
     ),
 
-                  
-                ],
-            ),
-            ),
-          ],
-        )
+                    
+                  ],
+              ),
+              ),
+            ],
+          ),
+        ]
       ),
       bottomNavigationBar: BottomNavigationBar(
           currentIndex: _currentIndex,
